65a090ae16cb50126bc11616b799fb07695204c3
Xavi Guasch <xaviguasch@gmail.com>
2025-01-03 22:14:55 +0100

    added 0003-anagrams--structy problem

---
+++ 0003-anagrams--structy/anagrams.js
@@ -0,0 +1,8 @@
+// anagrams
+// Write a function, anagrams, that takes in two strings as arguments. The function should return a boolean indicating whether or not the strings are anagrams. Anagrams are strings that contain the same characters, but in any order.
+
+const anagrams = (s1, s2) => {};
+
+//   anagrams('restful', 'fluster'); // -> true
+//   anagrams('cats', 'tocs'); // -> false
+//   anagrams('monkeyswrite', 'newyorktimes'); // -> true
