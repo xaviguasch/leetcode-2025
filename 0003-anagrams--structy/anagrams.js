7d770f21422270413086e21a6dd14bf77c7c9fef
Xavi Guasch <xaviguasch@gmail.com>
2025-01-04 19:35:43 +0100

    added solution to 0003-anagrams--structy problem

--- 0003-anagrams--structy/anagrams.js
+++ 0003-anagrams--structy/anagrams.js
@@ -1,7 +1,34 @@
 // anagrams
 // Write a function, anagrams, that takes in two strings as arguments. The function should return a boolean indicating whether or not the strings are anagrams. Anagrams are strings that contain the same characters, but in any order.
 
-const anagrams = (s1, s2) => {};
+const anagrams = (s1, s2) => {
+  const hashMap1 = {};
+  const hashMap2 = {};
+
+  for (const char of s1) {
+    if (!hashMap1[char]) {
+      hashMap1[char] = 1;
+    } else {
+      hashMap1[char]++;
+    }
+  }
+
+  for (const char of s2) {
+    if (!hashMap2[char]) {
+      hashMap2[char] = 1;
+    } else {
+      hashMap2[char]++;
+    }
+  }
+
+  for (key in hashMap2) {
+    if (hashMap2[key] !== hashMap1[key]) {
+      return false;
+    }
+  }
+
+  return true;
+};
 
 //   anagrams('restful', 'fluster'); // -> true
 //   anagrams('cats', 'tocs'); // -> false
