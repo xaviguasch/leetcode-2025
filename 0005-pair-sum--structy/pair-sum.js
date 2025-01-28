8008ed91f9996f7f7a72621f1f43aa0b1513061f
Xavi Guasch <xaviguasch@gmail.com>
2025-01-28 15:16:59 +0100

    added 0005-pair-sum--STRUCTY challenge

---
+++ 0005-pair-sum--structy/pair-sum.js
@@ -0,0 +1,12 @@
+// Write a function, pairSum, that takes in an array and a target sum as arguments. The function should return an array containing a pair of indices whose elements sum to the given target. The indices returned must be unique.
+
+// Be sure to return the indices, not the elements themselves.
+
+// There is guaranteed to be one such pair that sums to the target.
+
+const pairSum = (numbers, targetSum) => {
+  // todo
+};
+
+console.log(pairSum([3, 2, 5, 4, 1], 8));
+console.log(pairSum([4, 7, 9, 2, 5, 1], 5));
