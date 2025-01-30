f53c8eb8f79db2145ffe159897b03027b8f14cc6
Xavi Guasch <xaviguasch@gmail.com>
2025-01-30 20:23:56 +0100

    added 0007-uncompress--STRUCTY challenge

---
+++ 0007-uncompress--structy/uncompress.js
@@ -0,0 +1,10 @@
+// Write a function, uncompress, that takes in a string as an argument. The input string will be formatted into multiple groups according to the following pattern:
+
+// <number><char>
+
+// for example, '2c' or '3a'.
+// The function should return an uncompressed version of the string where each 'char' of a group is repeated 'number' times consecutively. You may assume that the input string is well-formed according to the previously mentioned pattern.
+
+const uncompress = (s) => {};
+
+console.log(uncompress("2c3a1t"));
