473e57064b64c8879eef48861582545c5daf6932
Xavi Guasch <xaviguasch@gmail.com>
2025-02-13 12:40:30 +0100

    added n01-reverse-string--grider challenge

---
+++ n01-reverse-string--grider/reverse-string.js
@@ -0,0 +1,15 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+  // write here
+}
+
+console.log(reverse("apple"));
+console.log(reverse("hello"));
+console.log(reverse("Greetings!"));
