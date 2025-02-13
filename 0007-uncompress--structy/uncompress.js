3927795a42918a9f4813f122030a8633b55dc1bc
Xavi Guasch <xaviguasch@gmail.com>
2025-02-13 18:25:02 +0100

    redid 0007-uncompress--STRUCTY from scratch

--- 0007-uncompress--structy/uncompress.js
+++ 0007-uncompress--structy/uncompress.js
@@ -7,26 +7,27 @@
 
 const uncompress = (s) => {
   const numbers = "0123456789";
-
   let result = [];
   let i = 0;
   let j = 0;
 
   while (j < s.length) {
     if (numbers.includes(s[j])) {
-      j += 1;
+      j++;
     } else {
       const num = Number(s.slice(i, j));
-
       for (let count = 0; count < num; count++) {
         result.push(s[j]);
       }
-      j += 1;
+      j++;
       i = j;
     }
   }
 
   return result.join("");
 };
+
+// Time O(N*M)
+// Space O(N*M)
 
 console.log(uncompress("2c3a1t"));
