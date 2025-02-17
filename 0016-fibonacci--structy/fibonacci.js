800bb3764235fee0b5dbc3855eba67c96b23e613
Xavi Guasch <xaviguasch@gmail.com>
2025-02-17 14:44:37 +0100

    added small adjustement to previous challenge

--- 0016-fibonacci--structy/fibonacci.js
+++ 0016-fibonacci--structy/fibonacci.js
@@ -11,12 +11,8 @@
 
 const fibonacci = (n) => {
   // todo
-  if (n === 0) {
-    return 0;
-  }
-
-  if (n === 1) {
-    return 1;
+  if (n === 0 || n === 1) {
+    return n;
   }
 
   return fibonacci(n - 1) + fibonacci(n - 2);
