47b07203d12aa2aeea4c2abc7ea827ec92a69033
Xavi Guasch <xaviguasch@gmail.com>
2025-02-17 14:33:51 +0100

    added 0016-fibonacci--structy challenge

---
+++ 0016-fibonacci--structy/fibonacci.js
@@ -0,0 +1,16 @@
+// fibonacci
+// Write a function, fibonacci, that takes in a number argument, n, and returns the n-th number of the Fibonacci sequence.
+
+// The 0-th number of the sequence is 0.
+
+// The 1-st number of the sequence is 1.
+
+// To generate further numbers of the sequence, calculate the sum of previous two numbers.
+
+// You must solve this recursively.
+
+const fibonacci = (n) => {
+  // todo
+};
+
+console.log(fibonacci(4));
