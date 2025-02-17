42b3cee4ed154eed1d4fb138cda83fd5ee78db5b
Xavi Guasch <xaviguasch@gmail.com>
2025-02-17 11:10:38 +0100

    added solution to previous challenge

--- 0015-palindrome-recursive--structy/palindrome-recursive.js
+++ 0015-palindrome-recursive--structy/palindrome-recursive.js
@@ -5,6 +5,18 @@
 
 const palindrome = (s) => {
   // todo
+  if (s.length <= 1) {
+    return true;
+  }
+
+  if (s[0] !== s[s.length - 1]) {
+    return false;
+  }
+
+  return palindrome(s.slice(1, s.length - 1));
 };
+
+// Time: O(n^2)
+// Space: O(n^2)
 
 console.log(palindrome("boot"));
