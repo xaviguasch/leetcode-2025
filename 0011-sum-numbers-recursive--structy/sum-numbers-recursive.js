d72981921036d3be577dc4d8be208651b8e47373
Xavi Guasch <xaviguasch@gmail.com>
2025-02-17 10:03:25 +0100

    added 0011-sum-numbers-recursive--STRUCTY

---
+++ 0011-sum-numbers-recursive--structy/sum-numbers-recursive.js
@@ -0,0 +1,7 @@
+// Write a function sumNumbersRecursive that takes in an array of numbers and returns the sum of all the numbers in the array. All elements will be integers. Solve this recursively.
+
+const sumNumbersRecursive = (numbers) => {
+  // TODO
+};
+
+console.log(sumNumbersRecursive([5, 2, 9, 10]));
