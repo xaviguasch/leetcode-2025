002dffab4904f40a4e179155ebe59a73b4c13825
Xavi Guasch <xaviguasch@gmail.com>
2025-02-18 11:30:05 +0100

    added 0017-linked-list-values--structy challenge

---
+++ 0017-linked-list-values--structy/linked-list-values.js
@@ -0,0 +1,24 @@
+// linked list values
+// Write a function, linkedListValues, that takes in the head of a linked list as an argument. The function should return an array containing all values of the nodes in the linked list.
+
+// Hey. This is our first linked list problem, so you should be liberal with watching the Approach and Walkthrough. Be productive! -AZ
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+const linkedListValues = (head) => {};
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+console.log(linkedListValues(a));
