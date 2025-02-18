3c94c33f98727e23b5166f6307ab4bc392597c77
Xavi Guasch <xaviguasch@gmail.com>
2025-02-18 17:11:09 +0100

    added recursive solution to previous challenge

--- 0021-reverse-list--structy/reverse-list.js
+++ 0021-reverse-list--structy/reverse-list.js
@@ -8,23 +8,36 @@ class Node {
   }
 }
 
-const reverseList = (head) => {
-  let prev = null;
-  let current = head;
+// ITERATIVE SOLUTION
+// const reverseList = (head) => {
+//   let prev = null;
+//   let current = head;
 
-  while (current !== null) {
-    const next = current.next;
-    current.next = prev;
-    prev = current;
-    current = next;
-  }
+//   while (current !== null) {
+//     const next = current.next;
+//     current.next = prev;
+//     prev = current;
+//     current = next;
+//   }
 
-  return prev;
-};
+//   return prev;
+// };
 
 // n = number of nodes
 // Time: O(n)
 // Space: O(1)
+
+// RECURSIVE SOLUTION
+const reverseList = (head, prev = null) => {
+  if (head === null) return prev;
+  const next = head.next;
+  head.next = prev;
+  return reverseList(next, head);
+};
+
+//   n = number of nodes
+//   Time: O(n)
+//   Space: O(n)
 
 const a = new Node("a");
 const b = new Node("b");
