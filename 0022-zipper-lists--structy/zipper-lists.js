71cb6314db2e016fbc6fe7313fd9a4121ddbc7de
Xavi Guasch <xaviguasch@gmail.com>
2025-02-19 09:52:09 +0100

    added iterative solution to previous challenge

--- 0022-zipper-lists--structy/zipper-lists.js
+++ 0022-zipper-lists--structy/zipper-lists.js
@@ -12,9 +12,41 @@ class Node {
   }
 }
 
+// ITERATIVE SOLUTION
 const zipperLists = (head1, head2) => {
   // todo
+  let tail = head1;
+  let current1 = head1.next;
+  let current2 = head2;
+  let count = 0;
+
+  while (current1 !== null && current2 !== null) {
+    if (count % 2 === 0) {
+      tail.next = current2;
+      current2 = current2.next;
+    }
+    if (count % 2 !== 0) {
+      tail.next = current1;
+      current1 = current1.next;
+    }
+    tail = tail.next;
+    count += 1;
+  }
+
+  if (current1 !== null) {
+    tail.next = current1;
+  }
+  if (current2 !== null) {
+    tail.next = current2;
+  }
+
+  return head1;
 };
+
+// n = length of list 1
+// m = length of list 2
+// Time: O(min(n, m))
+// Space: O(1)
 
 const a = new Node("a");
 const b = new Node("b");
