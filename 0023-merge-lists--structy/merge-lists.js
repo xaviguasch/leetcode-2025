d26c4902dadb6733846ec17283b62ee570d819c5
Xavi Guasch <xaviguasch@gmail.com>
2025-02-19 10:21:39 +0100

    added recursive solution to previous challenge

--- 0023-merge-lists--structy/merge-lists.js
+++ 0023-merge-lists--structy/merge-lists.js
@@ -12,33 +12,55 @@ class Node {
 }
 
 // ITERATIVE SOLUTION
-const mergeLists = (head1, head2) => {
-  let dummyHead = new Node(null);
-  let tail = dummyHead;
-  let current1 = head1;
-  let current2 = head2;
+// const mergeLists = (head1, head2) => {
+//   let dummyHead = new Node(null);
+//   let tail = dummyHead;
+//   let current1 = head1;
+//   let current2 = head2;
 
-  while (current1 !== null && current2 !== null) {
-    if (current1.val < current2.val) {
-      tail.next = current1;
-      current1 = current1.next;
-    } else {
-      tail.next = current2;
-      current2 = current2.next;
-    }
-    tail = tail.next;
-  }
+//   while (current1 !== null && current2 !== null) {
+//     if (current1.val < current2.val) {
+//       tail.next = current1;
+//       current1 = current1.next;
+//     } else {
+//       tail.next = current2;
+//       current2 = current2.next;
+//     }
+//     tail = tail.next;
+//   }
 
-  if (current1 !== null) tail.next = current1;
-  if (current2 !== null) tail.next = current2;
+//   if (current1 !== null) tail.next = current1;
+//   if (current2 !== null) tail.next = current2;
 
-  return dummyHead.next;
-};
+//   return dummyHead.next;
+// };
 
 // n = length of list 1
 // m = length of list 2
 // Time: O(min(n, m))
 // Space: O(1)
+
+// RECURSIVE VERSION
+const mergeLists = (head1, head2) => {
+  if (head1 === null && head2 === null) return null;
+  if (head1 === null) return head2;
+  if (head2 === null) return head1;
+
+  if (head1.val < head2.val) {
+    const next1 = head1.next;
+    head1.next = mergeLists(next1, head2);
+    return head1;
+  } else {
+    const next2 = head2.next;
+    head2.next = mergeLists(head1, next2);
+    return head2;
+  }
+};
+
+// n = length of list 1
+// m = length of list 2
+// Time: O(min(n, m))
+// Space: O(min(n, m))
 
 const a = new Node(5);
 const b = new Node(7);
