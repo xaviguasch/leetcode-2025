f406f11c1317bfb934c2a18b1c6b3a40a0b95be2
Xavi Guasch <xaviguasch@gmail.com>
2025-02-20 12:10:12 +0100

    added recursive solution to previous challenge

--- 0026-remove-node--structy/remove-node.js
+++ 0026-remove-node--structy/remove-node.js
@@ -15,30 +15,42 @@ class Node {
 }
 
 // ITERATIVE SOLUTION
-const removeNode = (head, targetVal) => {
-  // todo
-  if (head.val === targetVal) {
-    return head.next;
-  }
+// const removeNode = (head, targetVal) => {
+//   // todo
+//   if (head.val === targetVal) {
+//     return head.next;
+//   }
 
-  let current = head;
-  let prev = null;
+//   let current = head;
+//   let prev = null;
 
-  while (current !== null) {
-    if (current.val === targetVal) {
-      prev.next = current.next;
-      break;
-    }
-    prev = current;
-    current = current.next;
-  }
+//   while (current !== null) {
+//     if (current.val === targetVal) {
+//       prev.next = current.next;
+//       break;
+//     }
+//     prev = current;
+//     current = current.next;
+//   }
 
-  return head;
-};
+//   return head;
+// };
 
 // n = number of nodes
 // Time: O(n)
 // Space: O(1)
+
+// RECURSIVE SOLUTION
+const removeNode = (head, targetVal) => {
+  if (head === null) return null;
+  if (head.val === targetVal) return head.next;
+  head.next = removeNode(head.next, targetVal);
+  return head;
+};
+
+//   n = number of nodes
+// Time: O(n)
+// Space: O(n)
 
 const a = new Node("a");
 const b = new Node("b");
