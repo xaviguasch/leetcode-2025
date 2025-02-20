d7c0fc99eebd8fd5a4b3097e8ddef8c558f25b36
Xavi Guasch <xaviguasch@gmail.com>
2025-02-20 16:56:49 +0100

    added 0028-create-linked-list--structy challenge

---
+++ 0028-create-linked-list--structy/create-link-list.js
@@ -0,0 +1,13 @@
+// create linked list
+// Write a function, createLinkedList, that takes in an array of values as an argument. The function should create a linked list containing each element of the array as the values of the nodes. The function should return the head of the linked list.
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+const createLinkedList = (values) => {};
+
+console.log(createLinkedList(["h", "e", "y"]));
