bc40291f9398bf8912321732a168b9d8e469aa17
Xavi Guasch <xaviguasch@gmail.com>
2025-02-21 10:11:00 +0100

    added iterative solution to previous challenge

--- 0029-add-lists--structy/add-lists.js
+++ 0029-add-lists--structy/add-lists.js
@@ -18,27 +18,57 @@
 //     5 -> 7 -> 9
 
 // RECURSIVE SOLUTION
-const addLists = (head1, head2, carry = 0) => {
-  // todo
-  if (head1 === null && head2 === null && carry === 0) return null;
+// const addLists = (head1, head2, carry = 0) => {
+//   // todo
+//   if (head1 === null && head2 === null && carry === 0) return null;
 
-  const val1 = head1 === null ? 0 : head1.val;
-  const val2 = head2 === null ? 0 : head2.val;
+//   const val1 = head1 === null ? 0 : head1.val;
+//   const val2 = head2 === null ? 0 : head2.val;
 
-  const sum = val1 + val2 + carry;
-  const nextCarry = sum > 9 ? 1 : 0;
-  const digit = sum % 10;
-  const result = new Node(digit);
+//   const sum = val1 + val2 + carry;
+//   const nextCarry = sum > 9 ? 1 : 0;
+//   const digit = sum % 10;
+//   const result = new Node(digit);
 
-  const next1 = head1 === null ? null : head1.next;
-  const next2 = head2 === null ? null : head2.next;
+//   const next1 = head1 === null ? null : head1.next;
+//   const next2 = head2 === null ? null : head2.next;
 
-  result.next = addLists(next1, next2, nextCarry);
+//   result.next = addLists(next1, next2, nextCarry);
 
-  return result;
-};
+//   return result;
+// };
 
 // n = length of list 1
+// m = length of list 2
+// Time: O(max(n, m))
+// Space: O(max(n, m))
+
+// ITERATIVE SOLUTION
+const addLists = (head1, head2) => {
+  const dummyHead = new Node(null);
+  let tail = dummyHead;
+
+  let carry = 0;
+  let current1 = head1;
+  let current2 = head2;
+  while (current1 !== null || current2 !== null || carry !== 0) {
+    const val1 = current1 === null ? 0 : current1.val;
+    const val2 = current2 === null ? 0 : current2.val;
+    const sum = val1 + val2 + carry;
+    carry = sum > 9 ? 1 : 0;
+
+    const digit = sum % 10;
+    tail.next = new Node(digit);
+    tail = tail.next;
+
+    if (current1 !== null) current1 = current1.next;
+    if (current2 !== null) current2 = current2.next;
+  }
+
+  return dummyHead.next;
+};
+
+//   n = length of list 1
 // m = length of list 2
 // Time: O(max(n, m))
 // Space: O(max(n, m))
