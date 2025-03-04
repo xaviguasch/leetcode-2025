6535cec1bde96d486ddfc371008c5f89ded33ac8
Xavi Guasch <xaviguasch@gmail.com>
2025-03-04 20:04:06 +0100

    added recursive depth first solution to previous challenge

--- 0035-max-root-to-leaf-path-sum--structy/max-root-to-leaf-path-sum.js
+++ 0035-max-root-to-leaf-path-sum--structy/max-root-to-leaf-path-sum.js
@@ -11,9 +11,16 @@ class Node {
   }
 }
 
+// RECURSIVE DEPTH FIRST
 const maxPathSum = (root) => {
-  // todo
+  if (root === null) return -Infinity;
+  if (root.left === null && root.right === null) return root.val;
+  return root.val + Math.max(maxPathSum(root.left), maxPathSum(root.right));
 };
+
+// n = number of nodes
+// Time: O(n)
+// Space: O(n)
 
 const a = new Node(3);
 const b = new Node(11);
