512bb48d67bc15a664115c688b728866b366160b
Xavi Guasch <xaviguasch@gmail.com>
2025-03-13 11:11:24 +0100

    added s001-max-value--introduction--structy challenge

---
+++ s001-max-value--introduction--structy/max-value.js
@@ -0,0 +1,10 @@
+// max value
+// Write a function, maxValue, that takes in array of numbers as an argument. The function should return the largest number in the array.
+
+// Solve this without using any built-in array methods.
+
+// You can assume that the array is non-empty.
+
+const maxValue = (nums) => {};
+
+console.log(maxValue([4, 7, 2, 8, 10, 9]));
