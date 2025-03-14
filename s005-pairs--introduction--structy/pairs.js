438b6e9939a0568fe5fff426b2f8cdaf6977be7a
Xavi Guasch <xaviguasch@gmail.com>
2025-03-14 16:23:48 +0100

    added solution to previous challenge

--- s005-pairs--introduction--structy/pairs.js
+++ s005-pairs--introduction--structy/pairs.js
@@ -5,7 +5,21 @@
 
 // You can assume that the input array contains unique elements.
 
-const pairs = (elements) => {};
+const pairs = (elements) => {
+  const result = [];
+
+  for (let i = 0; i < elements.length; i++) {
+    for (let j = i + 1; j < elements.length; j++) {
+      result.push([elements[i], elements[j]]);
+    }
+  }
+
+  return result;
+};
+
+// n = length of array
+// Time: O(n^2)
+// Space: O(n^2)
 
 console.log(
   pairs(["cherry", "cranberry", "banana", "blueberry", "lime", "papaya"])
