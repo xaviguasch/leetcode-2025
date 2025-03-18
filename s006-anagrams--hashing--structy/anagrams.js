2fe6edd147f743dde7458c6491c0274aa3894944
Xavi Guasch <xaviguasch@gmail.com>
2025-03-18 14:22:47 +0100

    added s006-anagrams--hashing--structy challenge

---
+++ s006-anagrams--hashing--structy/anagrams.js
@@ -0,0 +1,11 @@
+// anagrams
+// Write a function, anagrams, that takes in two strings as arguments. The function should return a boolean indicating whether or not the strings are anagrams. Anagrams are strings that contain the same characters, but in any order.
+
+const anagrams = (s1, s2) => {};
+
+console.log(anagrams("restful", "fluster")); // -> true
+
+//   n = length of string 1
+//   m = length of string 2
+//   Time: O(n + m) , it's O(n) simplified
+//   Space: O(n + m) , it's O(n) simplified
