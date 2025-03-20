d7b212164ab4f2b0da98b4cf1ae9fc4d98083402
Xavi Guasch <xaviguasch@gmail.com>
2025-03-20 12:24:31 +0100

    added solution to previous challenge

--- s011-exclusive-items--hashing--structy/exclusive-items.js
+++ s011-exclusive-items--hashing--structy/exclusive-items.js
@@ -4,7 +4,29 @@
 // You may assume that each input array does not contain duplicate elements.
 
 const exclusiveItems = (a, b) => {
-  // todo
+  /  / / todo;
+  const hashMap = {};
+
+  const resultArr = [];
+
+  for (const num of a) {
+    hashMap[num] = 1;
+  }
+
+  for (const num of b) {
+    if (!hashMap[num]) {
+      hashMap[num] = 0;
+    }
+    hashMap[num] += 1;
+  }
+
+  for (const num in hashMap) {
+    if (hashMap[num] === 1) {
+      resultArr.push(Number(num));
+    }
+  }
+
+  return resultArr;
 };
 
 console.log(exclusiveItems([4, 2, 1, 6], [3, 6, 9, 2, 10])); // // -> [4,1,3,9,10]
