bf0badbe07d8e24a5f6f0291533345ece6f49b31
Xavi Guasch <xaviguasch@gmail.com>
2025-03-20 12:30:01 +0100

    added more optimal new solution to previous challenge

--- s011-exclusive-items--hashing--structy/exclusive-items.js
+++ s011-exclusive-items--hashing--structy/exclusive-items.js
@@ -4,29 +4,52 @@
 // You may assume that each input array does not contain duplicate elements.
 
 const exclusiveItems = (a, b) => {
-  /  / / todo;
-  const hashMap = {};
+  // todo;
+  //   const hashMap = {};
+
+  //   const resultArr = [];
+
+  //   for (const num of a) {
+  //     hashMap[num] = 1;
+  //   }
+
+  //   for (const num of b) {
+  //     if (!hashMap[num]) {
+  //       hashMap[num] = 0;
+  //     }
+  //     hashMap[num] += 1;
+  //   }
+
+  //   for (const num in hashMap) {
+  //     if (hashMap[num] === 1) {
+  //       resultArr.push(Number(num));
+  //     }
+  //   }
+
+  //   return resultArr;
+
+  const aSet = new Set(a);
+  const bSet = new Set(b);
 
   const resultArr = [];
 
   for (const num of a) {
-    hashMap[num] = 1;
+    if (!bSet.has(num)) {
+      resultArr.push(num);
+    }
   }
 
   for (const num of b) {
-    if (!hashMap[num]) {
-      hashMap[num] = 0;
-    }
-    hashMap[num] += 1;
-  }
-
-  for (const num in hashMap) {
-    if (hashMap[num] === 1) {
-      resultArr.push(Number(num));
+    if (!aSet.has(num)) {
+      resultArr.push(num);
     }
   }
 
   return resultArr;
 };
+
+// n = length of array a, m = length of array b
+// Time: O(n+m)
+// Space: O(n+m)
 
 console.log(exclusiveItems([4, 2, 1, 6], [3, 6, 9, 2, 10])); // // -> [4,1,3,9,10]
