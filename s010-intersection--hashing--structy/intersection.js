4b73a678eac2e92e4352593bf3253a99c7f4ac4d
Xavi Guasch <xaviguasch@gmail.com>
2025-03-20 11:58:54 +0100

    added solution to previous challenge

--- s010-intersection--hashing--structy/intersection.js
+++ s010-intersection--hashing--structy/intersection.js
@@ -3,6 +3,21 @@
 
 // You may assume that each input array does not contain duplicate elements.
 
-const intersection = (a, b) => {};
+const intersection = (a, b) => {
+  const numSet = new Set(a);
+  const resultArr = [];
+
+  for (const num of b) {
+    if (numSet.has(num)) {
+      resultArr.push(num);
+    }
+  }
+
+  return resultArr;
+};
+
+// n = length of array a, m = length of array b
+// Time: O(n*m)
+// Space: O(min(n,m))
 
 console.log(intersection([4, 2, 1, 6], [3, 6, 9, 2, 10])); // -> [2,6]
