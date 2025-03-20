56d2981d92e127fe1c78f124518c3b2ea1a4a1d2
Xavi Guasch <xaviguasch@gmail.com>
2025-03-20 12:01:15 +0100

    added s011-exclusive-items--hashing--structy challenge

---
+++ s011-exclusive-items--hashing--structy/exclusive-items.js
@@ -0,0 +1,10 @@
+// exclusive items
+// Write a function, exclusiveItems, that takes in two arrays, a,b, as arguments. The function should return a new array containing elements that are in either array but not both arrays.
+
+// You may assume that each input array does not contain duplicate elements.
+
+const exclusiveItems = (a, b) => {
+  // todo
+};
+
+console.log(exclusiveItems([4, 2, 1, 6], [3, 6, 9, 2, 10])); // // -> [4,1,3,9,10]
