45d6709cf7e9ed81cffac2ff7edb2af04a59d129
Xavi Guasch <xaviguasch@gmail.com>
2025-03-21 10:20:56 +0100

    added s013-intersection-with-dupes--hashing--structy challenge

---
+++ s013-intersection-with-dupes--hashing--structy/intersection-with-dupes.js
@@ -0,0 +1,9 @@
+// intersection with dupes
+// Write a function, intersectionWithDupes, that takes in two arrays, a,b, as arguments. The function should return a new array containing elements that are common to both input arrays. The elements in the result should appear as many times as they occur in both input arrays.
+
+// You can return the result in any order.
+
+const intersectionWithDupes = (a, b) => {};
+
+console.log(intersectionWithDupes(["a", "b", "c", "b"], ["x", "y", "b", "b"]));
+// -> ["b", "b"]
