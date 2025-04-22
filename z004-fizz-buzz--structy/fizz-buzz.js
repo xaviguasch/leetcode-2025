6b752b354eecdcdb06e0ccf2f694e83c481d177c
Xavi Guasch <xaviguasch@gmail.com>
2025-04-22 15:34:13 +0200

    added solution to previous challenge

--- z004-fizz-buzz--structy/fizz-buzz.js
+++ z004-fizz-buzz--structy/fizz-buzz.js
@@ -7,6 +7,25 @@
 
 const fizzBuzz = (n) => {
   // todo
+  const resultArr = [];
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      resultArr.push("fizzbuzz");
+    } else if (i % 3 === 0) {
+      resultArr.push("fizz");
+    } else if (i % 5 === 0) {
+      resultArr.push("buzz");
+    } else {
+      resultArr.push(i);
+    }
+  }
+
+  return resultArr;
 };
+
+// n = length of array
+// Time: O(n)
+// Space: O(n)
 
 console.log(fizzBuzz(16));
