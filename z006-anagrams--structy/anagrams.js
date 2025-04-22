d09c6ba4340aec1c8c4f117ad368f354c18a696d
Xavi Guasch <xaviguasch@gmail.com>
2025-04-22 17:06:37 +0200

    added solution to previous challenge

--- z006-anagrams--structy/anagrams.js
+++ z006-anagrams--structy/anagrams.js
@@ -3,6 +3,34 @@
 
 const anagrams = (s1, s2) => {
   // todo
+  const hashMap = {};
+
+  for (let char of s1) {
+    if (!hashMap[char]) {
+      hashMap[char] = 0;
+    }
+
+    hashMap[char] += 1;
+  }
+
+  for (let char of s2) {
+    if (hashMap[char]) {
+      hashMap[char] -= 1;
+    } else {
+      return false;
+    }
+  }
+
+  for (char in hashMap) {
+    if (hashMap[char] !== 0) {
+      return false;
+    }
+  }
+
+  return true;
 };
+
+// Time = O(n + m)
+// Space = O(n)
 
 console.log(anagrams("restful", "fluster"));
