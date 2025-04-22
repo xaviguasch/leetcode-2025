ab2dbceb5781d6d0c84baf044ab2c41d78b9cddf
Xavi Guasch <xaviguasch@gmail.com>
2025-04-22 16:44:45 +0200

    added z005-pairs--structy challenge

---
+++ z005-pairs--structy/pairs.js
@@ -0,0 +1,12 @@
+// pairs
+// Write a function, pairs, that takes in an array as an argument. The function should return an array contain all unique pairs of elements.
+
+// You may return the pairs in any order and the order of elements within a single pair does not matter.
+
+// You can assume that the input array contains unique elements.
+
+const pairs = (elements) => {
+  // todo
+};
+
+console.log(pairs(["a", "b", "c", "d"]));
