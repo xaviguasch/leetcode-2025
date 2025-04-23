037e26b144b14eaa3457ee160fa9bb5890e8d308
Xavi Guasch <xaviguasch@gmail.com>
2025-04-23 17:11:17 +0200

    added solution to previous challenge

--- z010-intersection--structy/intersection.js
+++ z010-intersection--structy/intersection.js
@@ -4,7 +4,17 @@
 // You may assume that each input array does not contain duplicate elements.
 
 const intersection = (a, b) => {
-  // todo
+  const result = [];
+  const setA = new Set(a);
+  for (let item of b) {
+    if (setA.has(item)) {
+      result.push(item);
+    }
+  }
+  return result;
 };
+
+// Time: O(N)
+// Space: O(N)
 
 console.log(intersection([4, 2, 1, 6], [3, 6, 9, 2, 10]));
