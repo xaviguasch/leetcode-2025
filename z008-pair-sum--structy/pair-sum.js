b6d09653d63368c5bd9c5ad2a699618ccbee9878
Xavi Guasch <xaviguasch@gmail.com>
2025-04-23 11:11:21 +0200

    added solution to previous challenge

--- z008-pair-sum--structy/pair-sum.js
+++ z008-pair-sum--structy/pair-sum.js
@@ -5,6 +5,22 @@
 
 // There is guaranteed to be one such pair that sums to the target.
 
-const pairSum = (numbers, targetSum) => {};
+const pairSum = (numbers, targetSum) => {
+  const hashMap = {};
+
+  for (const [index, num] of numbers.entries()) {
+    const complement = targetSum - num;
+
+    if (complement in hashMap) {
+      return [index, hashMap[complement]];
+    }
+
+    hashMap[num] = index;
+  }
+};
+
+// n = length of array
+// Time: O(n)
+// Space: O(n)
 
 console.log(pairSum([3, 2, 5, 4, 1], 8));
