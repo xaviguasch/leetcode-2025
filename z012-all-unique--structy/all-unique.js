f9d61b362d65ada357c004ef199dcd471407deaa
Xavi Guasch <xaviguasch@gmail.com>
2025-04-24 10:19:36 +0200

    added z012-all-unique--structy challenge

---
+++ z012-all-unique--structy/all-unique.js
@@ -0,0 +1,8 @@
+// all unique
+// Write a function, allUnique, that takes in an array. The function should return a boolean indicating whether or not the array contains unique items.
+
+const allUnique = (items) => {
+  // todo
+};
+
+console.log(allUnique(["q", "r", "s", "a"]));
