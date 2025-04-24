4ee236cd3a44d3af8a618d739baa33fda56f77d3
Xavi Guasch <xaviguasch@gmail.com>
2025-04-24 11:57:50 +0200

    added solution to previous challenge

--- z013-intersection-with-dupes--structy/intersection-with-dupes.js
+++ z013-intersection-with-dupes--structy/intersection-with-dupes.js
@@ -5,6 +5,41 @@
 
 const intersectionWithDupes = (a, b) => {
   // todo
+  const resultArr = [];
+
+  const hashMapA = {};
+  const hashMapB = {};
+
+  for (const item of a) {
+    if (!(item in hashMapA)) {
+      hashMapA[item] = 0;
+    }
+
+    hashMapA[item] += 1;
+  }
+
+  for (const item of b) {
+    if (!(item in hashMapB)) {
+      hashMapB[item] = 0;
+    }
+
+    hashMapB[item] += 1;
+  }
+
+  for (const key in hashMapA) {
+    if (key in hashMapB) {
+      const min = Math.min(hashMapA[key], hashMapB[key]);
+
+      for (let i = 0; i < min; i++) {
+        resultArr.push(key);
+      }
+    }
+  }
+
+  return resultArr;
 };
+
+// Space: O(N)
+// Time: O(N)
 
 console.log(intersectionWithDupes(["a", "b", "c", "b"], ["x", "y", "b", "b"]));
