e715429f5be41c2a36c3fee53545c7e787ee5e7c
Xavi Guasch <xaviguasch@gmail.com>
2025-04-24 10:21:13 +0200

    added solution to previous challenge

--- z012-all-unique--structy/all-unique.js
+++ z012-all-unique--structy/all-unique.js
@@ -2,7 +2,24 @@
 // Write a function, allUnique, that takes in an array. The function should return a boolean indicating whether or not the array contains unique items.
 
 const allUnique = (items) => {
-  // todo
+  // using sets
+  // const uniqueItems = new Set(items);
+  // return uniqueItems.size === items.length;
+
+  const hashMap = {};
+
+  for (const item of items) {
+    if (item in hashMap) {
+      return false;
+    }
+
+    hashMap[item] += 1;
+  }
+
+  return true;
 };
+
+// Time: O(N)
+// Space: O(N)
 
 console.log(allUnique(["q", "r", "s", "a"]));
