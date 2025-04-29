d1e5afc621b11e200fd1efd5446b2d0e4acd3e0f
Xavi Guasch <xaviguasch@gmail.com>
2025-04-29 15:02:34 +0200

    added z018-palindrome-recursive--structy challenge

---
+++ z018-palindrome-recursive--structy/palindrome-recursive.js
@@ -0,0 +1,10 @@
+// palindrome recursive
+// Write a function, palindrome, that takes in a string and returns a boolean indicating whether or not the string is the same forwards and backwards.
+
+// You must solve this recursively.
+
+const palindrome = (s) => {
+  // todo
+};
+
+console.log(palindrome("kayak"));
