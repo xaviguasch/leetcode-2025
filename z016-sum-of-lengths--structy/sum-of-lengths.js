c13d75fac6e9ed691c0da9fe254817ff1f28e7df
Xavi Guasch <xaviguasch@gmail.com>
2025-04-29 12:21:18 +0200

    added solution to previous challenge

--- z016-sum-of-lengths--structy/sum-of-lengths.js
+++ z016-sum-of-lengths--structy/sum-of-lengths.js
@@ -5,6 +5,14 @@
 
 const sumOfLengths = (strings) => {
   // todo
+  if (strings.length === 0) {
+    return 0;
+  }
+
+  return strings[0].length + sumOfLengths(strings.slice(1));
 };
+
+// Time: O(n^2)
+// Space: O(n^2)
 
 console.log(sumOfLengths(["goat", "cat", "purple"]));
