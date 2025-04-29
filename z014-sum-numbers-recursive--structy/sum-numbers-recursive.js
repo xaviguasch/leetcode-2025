f7a20833c0b6723b7bef903720206f28459e497a
Xavi Guasch <xaviguasch@gmail.com>
2025-04-29 11:34:48 +0200

    added solution to previous challenge

--- z014-sum-numbers-recursive--structy/sum-numbers-recursive.js
+++ z014-sum-numbers-recursive--structy/sum-numbers-recursive.js
@@ -3,6 +3,15 @@
 
 // Write a function sumNumbersRecursive that takes in an array of numbers and returns the sum of all the numbers in the array. All elements will be integers. Solve this recursively.
 
-const sumNumbersRecursive = (numbers) => {};
+const sumNumbersRecursive = (numbers) => {
+  if (numbers.length === 0) {
+    return 0;
+  }
+
+  return numbers[0] + sumNumbersRecursive(numbers.slice(1));
+};
+
+// Time: O(N^2)
+// Space: O(N^2)
 
 console.log(sumNumbersRecursive([5, 2, 9, 10]));
