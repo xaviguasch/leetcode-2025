42aacfedff5ffda0a67cbc1452f2b59fc5730a44
Xavi Guasch <xaviguasch@gmail.com>
2025-04-29 14:49:09 +0200

    added z017-reverse-string-recursive--structy challenge

---
+++ z017-reverse-string-recursive--structy/reverse-string-recursive.js
@@ -0,0 +1,8 @@
+// reverse string recursive
+// Write a function, reverseString, that takes in a string as an argument. The function should return the string with its characters in reverse order. You must do this recursively.
+
+const reverseString = (s) => {
+  // todo
+};
+
+console.log(reverseString("hello"));
