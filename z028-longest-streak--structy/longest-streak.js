63fa49fbea13fdd6f0d138e538851909d96ff0d5
Xavi Guasch <xaviguasch@gmail.com>
2025-05-03 13:36:40 +0200

    added z028-longest-streak--structy challenge

---
+++ z028-longest-streak--structy/longest-streak.js
@@ -0,0 +1,28 @@
+// longest streak
+// Write a function, longestStreak, that takes in the head of a linked list as an argument. The function should return the length of the longest consecutive streak of the same value within the list.
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+const longestStreak = (head) => {};
+
+const a = new Node(5);
+const b = new Node(5);
+const c = new Node(7);
+const d = new Node(7);
+const e = new Node(7);
+const f = new Node(6);
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+e.next = f;
+
+// 5 -> 5 -> 7 -> 7 -> 7 -> 6
+
+console.log(longestStreak(a)); // 3
