a0e10c2d1588d100d7e36f97cef395825907e1aa
Xavi Guasch <xaviguasch@gmail.com>
2025-05-05 11:04:44 +0200

    added iterative solution to previous challenge

--- z029-remove-node--structy/remove-node.js
+++ z029-remove-node--structy/remove-node.js
@@ -14,7 +14,27 @@ class Node {
   }
 }
 
-const removeNode = (head, targetVal) => {};
+const removeNode = (head, targetVal) => {
+  // ITERATIVE
+  if (head.val === targetVal) return head.next;
+
+  let current = head;
+  let prev = null;
+  while (current !== null) {
+    if (current.val === targetVal) {
+      prev.next = current.next;
+      break;
+    }
+    prev = current;
+    current = current.next;
+  }
+
+  return head;
+
+  //     n = number of nodes
+  // Time: O(n)
+  // Space: O(1)
+};
 
 const a = new Node("a");
 const b = new Node("b");
