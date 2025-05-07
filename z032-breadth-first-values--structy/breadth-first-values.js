ed41b52bef90354d9318691d3e76f5e211b08057
Xavi Guasch <xaviguasch@gmail.com>
2025-05-07 12:43:17 +0200

    added solution to previous challenge

--- z032-breadth-first-values--structy/breadth-first-values.js
+++ z032-breadth-first-values--structy/breadth-first-values.js
@@ -11,6 +11,25 @@ class Node {
 
 const breadthFirstValues = (root) => {
   // ITERATIVE
+  if (root === null) return [];
+
+  const values = [];
+  const queue = [root];
+
+  while (queue.length > 0) {
+    const node = queue.shift();
+    values.push(node.val);
+
+    if (node.left !== null) queue.push(node.left);
+
+    if (node.right !== null) queue.push(node.right);
+  }
+
+  return values;
+
+  //   n = number of nodes
+  // Time: O(n)
+  // Space: O(n)
 };
 
 const a = new Node("a");
