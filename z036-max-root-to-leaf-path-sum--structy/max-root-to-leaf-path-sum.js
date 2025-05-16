fc06228b5c8c4b57d9b4ca1f47bfafc1cbec12f7
Xavi Guasch <xaviguasch@gmail.com>
2025-05-16 18:07:45 +0200

    added solution to previous challenge

--- z036-max-root-to-leaf-path-sum--structy/max-root-to-leaf-path-sum.js
+++ z036-max-root-to-leaf-path-sum--structy/max-root-to-leaf-path-sum.js
@@ -12,7 +12,14 @@ class Node {
 }
 
 const maxPathSum = (root) => {
-  // todo
+  // RECURSIVE DEPTH FIRST
+  if (root === null) return -Infinity;
+  if (root.left === null && root.right === null) return root.val;
+  return root.val + Math.max(maxPathSum(root.left), maxPathSum(root.right));
+
+  //   n = number of nodes
+  // Time: O(n)
+  // Space: O(n)
 };
 
 const a = new Node(3);
