ffb1bdba5361b1c7eed3c3eb1dd38ca82fd8db3d
Xavi Guasch <xaviguasch@gmail.com>
2025-05-21 15:15:33 +0200

    added recursive depth first solution to the previous challenge

--- z039-how-high--structy/how-high.js
+++ z039-how-high--structy/how-high.js
@@ -15,6 +15,15 @@ class Node {
 
 const howHigh = (node) => {
   // todo
+  if (node === null) return -1;
+
+  const leftHeight = howHigh(node.left);
+  const rightHeight = howHigh(node.right);
+  return 1 + Math.max(leftHeight, rightHeight);
+
+  //   n = number of nodes
+  // Time: O(n)
+  // Space: O(n)
 };
 
 const a = new Node("a");
