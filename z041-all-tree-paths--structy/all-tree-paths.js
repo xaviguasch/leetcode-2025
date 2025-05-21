abe0ec8411d5e97336e84156fe4a108e3cce4074
Xavi Guasch <xaviguasch@gmail.com>
2025-05-21 17:37:04 +0200

    added z041-all-tree-paths--structy challenge

---
+++ z041-all-tree-paths--structy/all-tree-paths.js
@@ -0,0 +1,44 @@
+// all tree paths
+// Write a function, allTreePaths, that takes in the root of a binary tree. The function should return a 2-Dimensional array where each subarray represents a root-to-leaf path in the tree.
+
+// The order within an individual path must start at the root and end at the leaf, but the relative order among paths in the outer array does not matter.
+
+// You may assume that the input tree is non-empty.
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const allTreePaths = (root) => {
+  // todo
+};
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+
+console.log(allTreePaths(a)); // ->
+// [
+//   [ 'a', 'b', 'd' ],
+//   [ 'a', 'b', 'e' ],
+//   [ 'a', 'c', 'f' ]
+// ]
