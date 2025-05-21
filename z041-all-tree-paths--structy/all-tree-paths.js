0d0f9925430449624534691b99bcdfc728338577
Xavi Guasch <xaviguasch@gmail.com>
2025-05-21 17:59:25 +0200

    added recursive depth first solution to the previous challenge

--- z041-all-tree-paths--structy/all-tree-paths.js
+++ z041-all-tree-paths--structy/all-tree-paths.js
@@ -14,8 +14,42 @@ class Node {
 }
 
 const allTreePaths = (root) => {
-  // todo
+  const paths = _allTreePaths(root);
+  for (let path of paths) {
+    path.reverse();
+  }
+  return paths;
 };
+
+const _allTreePaths = (root) => {
+  if (root === null) {
+    return [];
+  }
+
+  if (root.left === null && root.right === null) {
+    return [[root.val]];
+  }
+
+  const allPaths = [];
+
+  const leftSubPaths = _allTreePaths(root.left);
+  for (let path of leftSubPaths) {
+    path.push(root.val);
+    allPaths.push(path);
+  }
+
+  const rightSubPaths = _allTreePaths(root.right);
+  for (let path of rightSubPaths) {
+    path.push(root.val);
+    allPaths.push(path);
+  }
+
+  return allPaths;
+};
+
+// n = number of nodes
+// Time: O( n*log(n) )
+// Space: O( n*log(n) )
 
 const a = new Node("a");
 const b = new Node("b");
