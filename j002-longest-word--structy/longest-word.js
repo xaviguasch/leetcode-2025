e8bcaf39db54bcd43d5f96e9d6cb30a06f8ed873
Xavi Guasch <xaviguasch@gmail.com>
2025-07-16 18:33:33 +0200

    solved previous exercise

--- j002-longest-word--structy/longest-word.js
+++ j002-longest-word--structy/longest-word.js
@@ -2,6 +2,22 @@
 // Write a function, longestWord, that takes in a sentence string as an argument. The function should return the longest word in the sentence. If there is a tie, return the word that occurs later in the sentence.
 
 // You can assume that the sentence is non-empty.
-const longestWord = (sentence) => {};
+const longestWord = (sentence) => {
+  const newArr = sentence.split(" ");
+
+  let longestW = "";
+
+  for (let word of newArr) {
+    if (word.length >= longestW.length) {
+      longestW = word;
+    }
+  }
+
+  return longestW;
+};
+
+// n = length of sentence
+// Time: O(n)
+// Space: O(n)
 
 console.log(longestWord("what a wonderful world")); // -> 'wonderful'
