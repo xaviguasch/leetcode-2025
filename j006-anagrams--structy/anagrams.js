f1a4494d19d987f24f967937622b89be165cab96
Xavi Guasch <xaviguasch@gmail.com>
2025-07-17 12:02:38 +0200

    solved previous exercise

--- j006-anagrams--structy/anagrams.js
+++ j006-anagrams--structy/anagrams.js
@@ -1,6 +1,36 @@
 // anagrams
 // Write a function, anagrams, that takes in two strings as arguments. The function should return a boolean indicating whether or not the strings are anagrams. Anagrams are strings that contain the same characters, but in any order.
 
-const anagrams = (s1, s2) => {};
+const anagrams = (s1, s2) => {
+  const count = {};
+
+  for (let char of s1) {
+    if (!(char in count)) {
+      count[char] = 0;
+    }
+    count[char] += 1;
+  }
+
+  for (let char of s2) {
+    if (char in count) {
+      count[char] -= 1;
+    } else {
+      return false;
+    }
+  }
+
+  for (let char in count) {
+    if (count[char] !== 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+// n = length of string 1
+// m = length of string 2
+// Time: O(n + m)
+// Space: O(n)
 
 console.log(anagrams("monkeyswrite", "newyorktimes")); // -> true
