a7eff1f6d684d96cc00ab750636978176d7d9a84
Xavi Guasch <xaviguasch@gmail.com>
2025-07-22 16:01:11 +0200

    added solution to previous challenge

--- j007-most-frequent-char--structy/most-frequent-char.js
+++ j007-most-frequent-char--structy/most-frequent-char.js
@@ -5,6 +5,29 @@
 
 const mostFrequentChar = (s) => {
   // todo
+  const hashMap = {};
+  let mostFreqChar = "";
+  let numberMostFreq = 0;
+
+  for (let char of s) {
+    if (!(char in hashMap)) {
+      hashMap[char] = 0;
+    }
+    hashMap[char] += 1;
+  }
+
+  for (let char in hashMap) {
+    if (hashMap[char] > numberMostFreq) {
+      numberMostFreq = hashMap[char];
+      mostFreqChar = char;
+    }
+  }
+
+  return mostFreqChar;
 };
+
+// n = length of string
+// Time: O(n)
+// Space: O(n)
 
 console.log(mostFrequentChar("bookeeper")); // -> 'e'
