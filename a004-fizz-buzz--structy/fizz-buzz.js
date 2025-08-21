1d50be635f107ce207f90bb8a0ce8e482b8f2a80
Xavi Guasch <xaviguasch@gmail.com>
2025-08-21 17:17:57 +0200

    added a004-fizz-buzz--structy challenge

---
+++ a004-fizz-buzz--structy/fizz-buzz.js
@@ -0,0 +1,12 @@
+// fizz buzz
+// Write a function, fizzBuzz, that takes in a number n as an argument. The function should return an array containing numbers from 1 to n, replacing certain numbers according to the following rules:
+
+// if the number is divisible by 3, make the element "fizz"
+// if the number is divisible by 5, make the element "buzz"
+// if the number is divisible by 3 and 5, make the element "fizzbuzz"
+
+const fizzBuzz = (n) => {
+  // todo
+};
+
+console.log(fizzBuzz(11)); // -> [1,2,"fizz",4,"buzz","fizz",7,8,"fizz","buzz",11]
