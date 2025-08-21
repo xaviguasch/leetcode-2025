c91335d23e48337676b9521b85da58adc43ddad4
Xavi Guasch <xaviguasch@gmail.com>
2025-08-21 16:53:35 +0200

    added a002-longest-word--structy challenge

---
+++ a002-longest-word--structy/longest-word.js
@@ -0,0 +1,10 @@
+// longest word
+// Write a function, longestWord, that takes in a sentence string as an argument. The function should return the longest word in the sentence. If there is a tie, return the word that occurs later in the sentence.
+
+// You can assume that the sentence is non-empty.
+
+const longestWord = (sentence) => {
+  // todo
+};
+
+console.log(longestWord("what a wonderful world")); // -> 'wonderful'
