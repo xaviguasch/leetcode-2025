74378d2390bcc918f3a0bb561573082ac046ab35
Xavi Guasch <xaviguasch@gmail.com>
2025-08-26 11:27:00 +0200

    added solution to previous challenge

--- x006-anagrams--structy/anagrams.js
+++ x006-anagrams--structy/anagrams.js
@@ -3,11 +3,35 @@
 
 const anagrams = (s1, s2) => {
   // todo
+  const count = {};
+
+  for (let char of s1) {
+    if (!(char in count)) {
+      count[char] = 0;
+    }
+    count[char] += 1;
+  }
+
+  for (let char of s2) {
+    if (char in count) {
+      count[char] -= 1;
+    } else {
+      return false;
+    }
+  }
+
+  for (let char in count) {
+    if (count[char] !== 0) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
-console.log(anagrams("restful", "fluster")); // -> true
+console.log(anagrams("paper", "reapa")); // -> false
 
 // n = length of string 1
 // m = length of string 2
-// Time: O()
-// Space: O()
+// Time: O(n)
+// Space: O(n)
