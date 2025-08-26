d9218e9cfdcd7aab5000253eef94ea6491a85205
Xavi Guasch <xaviguasch@gmail.com>
2025-08-26 17:01:49 +0200

    added solution to previous challenge

--- x007-most-frequent-char--structy/most-frequent-char.js
+++ x007-most-frequent-char--structy/most-frequent-char.js
@@ -3,6 +3,26 @@
 
 // You can assume that the input string is non-empty.
 
-const mostFrequentChar = (s) => {};
+const mostFrequentChar = (s) => {
+  const count = {};
+
+  for (let char of s) {
+    if (!(char in count)) {
+      count[char] = 0;
+    }
+    count[char] += 1;
+  }
+
+  let best = null;
+  for (let char of s) {
+    if (best === null || count[char] > count[best]) {
+      best = char;
+    }
+  }
+  return best;
+};
+
+// Time: O(N)
+// Space: O(N)
 
 console.log(mostFrequentChar("bookeeper")); // -> 'e'
