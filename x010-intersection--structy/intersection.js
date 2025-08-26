ca867b87f864ddf962d38fbafed8e65a7e1295e4
Xavi Guasch <xaviguasch@gmail.com>
2025-08-26 20:16:35 +0200

    added x010-intersection--structy challenge

---
+++ x010-intersection--structy/intersection.js
@@ -0,0 +1,13 @@
+// intersection
+// Write a function, intersection, that takes in two arrays, a,b, as arguments. The function should return a new array containing elements that are in both of the two arrays.
+
+// You may assume that each input array does not contain duplicate elements.
+
+const intersection = (a, b) => {
+  // todo
+};
+
+// Time: O()
+// Space: O()
+
+console.log(intersection([4, 2, 1, 6], [3, 6, 9, 2, 10])); // -> [2,6]
