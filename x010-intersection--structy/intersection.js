eca499de30894b33476459b0e360fb70555127a4
Xavi Guasch <xaviguasch@gmail.com>
2025-08-26 20:23:44 +0200

    added solution to previous challenge

--- x010-intersection--structy/intersection.js
+++ x010-intersection--structy/intersection.js
@@ -5,9 +5,37 @@
 
 const intersection = (a, b) => {
   // todo
+  //   const hashMap = {}
+
+  //   const resultArr = []
+
+  //   for (const num of a) {
+  //     hashMap[num] = 1
+  //   }
+
+  //   for (const num of b) {
+  //     if (num in hashMap) {
+  //       resultArr.push(num)
+  //     }
+  //   }
+
+  //   return resultArr
+
+  const resultArr = [];
+
+  const setA = new Set(a);
+
+  for (let item of b) {
+    if (setA.has(item)) {
+      resultArr.push(item);
+    }
+  }
+
+  return resultArr;
 };
 
-// Time: O()
-// Space: O()
+// n = length of array a, m = length of array b
+// Time: O(n+m)
+// Space: O(n)
 
 console.log(intersection([4, 2, 1, 6], [3, 6, 9, 2, 10])); // -> [2,6]
