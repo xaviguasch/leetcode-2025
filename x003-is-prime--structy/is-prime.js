8951eddaf5279bc8f5972897b4078dcb51899005
Xavi Guasch <xaviguasch@gmail.com>
2025-08-26 10:31:55 +0200

    added solution to previous challenge

--- x003-is-prime--structy/is-prime.js
+++ x003-is-prime--structy/is-prime.js
@@ -9,10 +9,19 @@
 
 const isPrime = (n) => {
   // todo
+  if (n < 2) return false;
+
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
-// n = input number
-// Time:
-// Space:
-
 console.log(isPrime(6)); // -> false
+
+// n = input number
+// Time: O(n)
+// Space: O(1)
