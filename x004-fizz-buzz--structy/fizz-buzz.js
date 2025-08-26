48432d2c36edef95b7feb1636826f4e6e940605f
Xavi Guasch <xaviguasch@gmail.com>
2025-08-26 10:40:48 +0200

    added solution to previous challenge

--- x004-fizz-buzz--structy/fizz-buzz.js
+++ x004-fizz-buzz--structy/fizz-buzz.js
@@ -6,11 +6,24 @@
 // if the number is divisible by 3 and 5, make the element "fizzbuzz"
 
 const fizzBuzz = (n) => {
-  // todo
+  const finalArr = [];
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      finalArr.push("fizzbuzz");
+    } else if (i % 3 === 0) {
+      finalArr.push("fizz");
+    } else if (i % 5 === 0) {
+      finalArr.push("buzz");
+    } else {
+      finalArr.push(i);
+    }
+  }
+
+  return finalArr;
 };
 
-// n = input number
-// Time:
-// Space:
+// Time: O(N)
+// Space: O(N)
 
 console.log(fizzBuzz(11)); // -> [1,2,"fizz",4,"buzz","fizz",7,8,"fizz","buzz",11]
