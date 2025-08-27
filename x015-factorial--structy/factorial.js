fd803420a08d8c3f2c73ef6c4cea817d84cc7082
Xavi Guasch <xaviguasch@gmail.com>
2025-08-27 18:33:18 +0200

    added x015-factorial--structy challenge

---
+++ x015-factorial--structy/factorial.js
@@ -0,0 +1,16 @@
+// factorial
+// Write a function, factorial, that takes in a number n and returns the factorial of that number. The factorial of n is the product of all the positive numbers less than or equal to n. You must solve this recursively.
+
+// For example, the factorial of 6 is:
+
+// 6 * 5 * 4 * 3 * 2 * 1 = 720
+// You can assume that n is a non-negative integer. Note that the factorial of 0 is defined to be 1 (wiki).
+
+const factorial = (n) => {
+  // todo
+};
+
+// Time:
+// Space:
+
+console.log(factorial(3)); // -> 6
