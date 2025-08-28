8c41c8e741026179c523250c70a29d9d45446cef
Xavi Guasch <xaviguasch@gmail.com>
2025-08-28 12:28:11 +0200

    added solution to previous challenge

--- x016-sum-of-lengths--structy/sum-of-lengths.js
+++ x016-sum-of-lengths--structy/sum-of-lengths.js
@@ -5,9 +5,14 @@
 
 const sumOfLengths = (strings) => {
   // todo
+  if (strings.length === 0) {
+    return 0;
+  }
+
+  return strings[0].length + sumOfLengths(strings.slice(1));
 };
 
 console.log(sumOfLengths(["goat", "cat", "purple"]));
 
-// Time:
-// Space:
+// Time: O(N^2)
+// Space: O(N^2)
