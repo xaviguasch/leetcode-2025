2922abb051273cf79e84640a71eda5478b4b16e9
Xavi Guasch <xaviguasch@gmail.com>
2025-08-28 12:23:52 +0200

    added x016-sum-of-lengths--structy challenge

---
+++ x016-sum-of-lengths--structy/sum-of-lengths.js
@@ -0,0 +1,13 @@
+// sum of lengths
+// Write a function sumOfLengths that takes in array of strings and returns the total length of the strings.
+
+// You must solve this recursively.
+
+const sumOfLengths = (strings) => {
+  // todo
+};
+
+console.log(sumOfLengths(["goat", "cat", "purple"]));
+
+// Time:
+// Space:
