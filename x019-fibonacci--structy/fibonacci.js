3e687ab670fc0d123575bea7518272d66b1e7815
Xavi Guasch <xaviguasch@gmail.com>
2025-08-29 13:09:32 +0200

    added solution to previous challenge

--- x019-fibonacci--structy/fibonacci.js
+++ x019-fibonacci--structy/fibonacci.js
@@ -11,9 +11,18 @@
 
 const fibonacci = (n) => {
   // todo
+  if (n <= 0) {
+    return 0;
+  }
+
+  if (n === 1) {
+    return 1;
+  }
+
+  return fibonacci(n - 2) + fibonacci(n - 1);
 };
 
-// Time:
-// Space:
+// Time: O(2^n)
+// Space: O(n)
 
 console.log(fibonacci(3)); // -> 2
