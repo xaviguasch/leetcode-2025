db2be819a3d21e9869dcd7c2505ea0242fd53258
Xavi Guasch <xaviguasch@gmail.com>
2025-08-29 12:58:45 +0200

    added solution to previous challenge

--- x018-palindrome-recursive--structy/palindrome-recursive.js
+++ x018-palindrome-recursive--structy/palindrome-recursive.js
@@ -5,10 +5,19 @@
 
 const palindrome = (s) => {
   // todo
+  if (s.length <= 1) {
+    return true;
+  }
+
+  if (s[0] !== s[s.length - 1]) {
+    return false;
+  }
+
+  return palindrome(s.slice(1, -1));
 };
 
-// Time:
-// Space:
+// Time: O(N^2)
+// Space: O(N^2)
 
 console.log(palindrome("pop"));
 // -> true
