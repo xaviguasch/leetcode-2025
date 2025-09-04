a68adef4d9f78643d694caac2372d8e87db31360
Xavi Guasch <xaviguasch@gmail.com>
2025-09-04 12:32:09 +0200

    added x024-reverse-list--structy challenge

---
+++ x024-reverse-list--structy/reverse-list.js
@@ -0,0 +1,36 @@
+// reverse list
+// Write a function, reverseList, that takes in the head of a linked list as an argument. The function should reverse the order of the nodes in the linked list in-place and return the new head of the reversed linked list.
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+const reverseList = (head) => {
+  // ITERATIVE
+  // Time: O()
+  // Space: O()
+  // RECURSIVE
+  // Time: O()
+  // Space: O()
+};
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+e.next = f;
+
+// a -> b -> c -> d -> e -> f
+
+console.log(reverseList(a));
+// f -> e -> d -> c -> b -> a
